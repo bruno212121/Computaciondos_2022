9bac9d6e3b5c375cc60a616c7a1fce9feeeb5f45
BrunoRosales <brunonicorc@hotmail.com>
2022-06-21 22:53:25 -0300

    end

--- threading/th_rot13.py
+++ threading/th_rot13.py
@@ -1,39 +1,23 @@
-import codecs
-import queue
-import threading as th, sys, os
+import threading as th, sys, os, codecs
 
-def hilo(w, q):
-    sys.stdin = open(0)
+def read_sys(w):
     line = sys.stdin.readline()
     os.write(w, line.encode('ascii'))
-    line = q.get()
-    q.task_done()
-    print(f'H1 indent: {th.current_thread().ident} recupera la linea encriptada desde la cola de mensajes: ({line[:-1]})')
 
-
-def hilo_uno(q, r):
+def rot(r):
     line = os.read(r, 100).decode()
-    line = rot13(line)
-    q.put(line)
-    q.join()
-
-def rot13(line):
     line_rot13 = codecs.encode(line, 'rot_13')
-    return line_rot13
-
-def main():
-    w, r = os.pipe()
-    q = queue.LifoQueue()
-
-    hilo1 = th.Thread(target=hilo, args=(w, q))
-    hilodos= th.Thread(target=hilo_uno, args=(q, r))
-
-    hilo1.start()
-    hilodos.start()
-
-    hilo1.join()
-    hilodos.join()
+    print(f'Texto cifrado: {line_rot13}')
 
 if __name__ == '__main__':
-    main()
+    r, w = os.pipe()
+
+    hilo = th.Thread(target=read_sys, args=(w,))
+    hilodos= th.Thread(target=rot, args=(r,))
+
+    hilo.start()
+    hilodos.start()
+
+    hilo.join()
+    hilodos.join()
 
