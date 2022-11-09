e6a41576bcc9a6d73f415db15aced6b143066f07
BrunoRosales <brunonicorc@hotmail.com>
2022-11-08 22:12:26 -0300

    celery_finish

--- celery/func_matriz.py
+++ celery/func_matriz.py
@@ -1,41 +1,7 @@
-"""
-Realizar un programa en python que reciba por argumentos:
-
--f /ruta/al/archivo_matriz.txt
-
--c funcion_calculo
-El programa deberá leer una matriz almacenada en el archivo de texto pasado por argumento -f, y 
-deberá calcular la funcion_calculo para cada uno de sus elementos.
-
-Para aumentar la performance, el programa utilizará un Celery, 
-que recibirá mediante una cola de mensajes Redis, cada una de las tareas a ejecutar.
-
-La funcion_calculo, modelada como tareas de Celery, podrá ser una de las siguientes:
-
-raiz: calcula la raíz cuadrada del elemento.
-pot: calcula la potencia del elemento elevado a si mismo.
-log: calcula el logaritmo decimal de cada elemento.
-
-"""
 import os
 import sys
-import click, socket
-from celery import Celery
-from math import sqrt, log10, pow
-
-app = Celery('func_matriz', broker='redis://localhost:6379', backend='redis://localhost:6379') 
-
-@app.task
-def pot(element):
-    return pow(element, element)
-
-@app.task
-def raiz(element):
-    return sqrt.int(element)
-
-@app.task
-def log(element):
-    return log10.int(element)
+import click
+from celery_config import pot, log, raiz
 
 @click.command()
 @click.option('-f', '--file', 'file', help='Ruta al archivo de texto', required=True)
@@ -50,14 +16,16 @@ def main(file, calc):
         sys.exit(1)
     with open(file, 'r') as f:
         for line in f:
-            for num in line.split():
+            # print(line)
+            for num in line.split(","):
+                print(f" numero de la matriz :{num}")
                 if calc == 'raiz':
-                    result = app.send_task('func_matriz.raiz', args=[num])
+                    resultado = raiz(int(num))
                 elif calc == 'pot':
-                    result = app.send_task('func_matriz.pot', args=[num])
+                    resultado = pot(int(num))
                 elif calc == 'log':
-                    result = app.send_task('func_matriz.log', args=[num])
-                print(result.get())
+                    resultado = log(int(num))
+                print(resultado)
 
 if __name__=="__main__":
     main()
