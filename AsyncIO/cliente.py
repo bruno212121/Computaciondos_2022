fa48b2a0c839dcf10704ed362eeff4b1e59ea776
BrunoRosales <brunonicorc@hotmail.com>
2022-11-18 13:29:28 -0300

    remote_shell

--- AsyncIO/cliente.py
+++ AsyncIO/cliente.py
@@ -1,21 +1,26 @@
-import asyncio
+import asyncio, argparse, subprocess
 
-async def tcp_echo_client(message):
-    reader, writer = await asyncio.open_connection(
-        '127.0.0.1', 1234)
+parser = argparse.ArgumentParser(description="Servidor asincrónico")
+parser.add_argument('-ht', type=str, help="Ingresar host")
+parser.add_argument('-p', type=int, help="Ingresar puerto")
+args = parser.parse_args()
 
-    print(reader, writer)
-    print(f'Send: {message!r}')
-    writer.write(message.encode())
-    await writer.drain()
-
-    data = await reader.read(100)
-    print(f'Received: {data.decode()!r}')
-
-    print('Close the connection')
-    writer.close()
-    print(asyncio.current_task())
-    await writer.wait_closed()
-    
-
-asyncio.run(tcp_echo_client('Hello World!'))
+async def tcp_echo_client():
+    reader, writer = await asyncio.open_connection(args.ht, args.p)
+    while True:
+        command = input("> ")
+        salida = command
+        command = command.encode()
+        if salida[:4] == "exit" or len(salida) == 0:
+            print("Saliendo...")
+            writer.close()
+            await writer.wait_closed()
+            return
+        writer.write(command)
+        await writer.drain()
+        recv = await reader.read(1024)
+        print(recv.decode())
+try:
+    asyncio.run(tcp_echo_client())
+except ConnectionResetError as e:
+    print(e)
